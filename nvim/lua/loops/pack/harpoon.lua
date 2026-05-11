292b35357b4d3056f2afa1e026c1de4bacc3f8a2
weihanyau <weihanyau@gmail.com>
2026-05-12 00:27:26 +0800

    add prev and next keybind for harpoon

--- nvim/lua/loops/pack/harpoon.lua
+++ nvim/lua/loops/pack/harpoon.lua
@@ -25,3 +25,7 @@ vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
