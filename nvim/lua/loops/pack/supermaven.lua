8a7a0789abe24339619839ad28d31f5eb7d5337f
weihanyau <weihanyau@gmail.com>
2026-05-31 22:20:15 +0800

    add supermaven

---
+++ nvim/lua/loops/pack/supermaven.lua
@@ -0,0 +1,9 @@
+vim.pack.add({ "https://github.com/supermaven-inc/supermaven-nvim" })
+
+require("supermaven-nvim").setup({
+    keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+    },
+})
