f2bc979d56ea6f82fc037b6ca7da88b54f214a17
weihanyau <weihanyau@gmail.com>
2026-05-31 21:56:48 +0800

    add bufenter to nvim lint

--- nvim/lua/loops/pack/lsp.lua
+++ nvim/lua/loops/pack/lsp.lua
@@ -81,7 +81,7 @@ require("lint").linters_by_ft = {
 	python = { "ruff" },
 }
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
 	callback = function()
 		require("lint").try_lint()
 	end,
